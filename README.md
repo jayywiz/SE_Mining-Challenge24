f428391f0f20fc21beb0849a6c470894576a79a3
Akhil Raj Tirumalasetty <akhilrajtirumalasetty@gmail.com>
2023-11-22 00:05:32 -0500

    Add Script: Truncate Dataset
    Truncating large dataset into smaller chunks and integrated to our
    python code.
    
    Overall Progress:
    We also used Powershell script to truncate a large dataset and filter into smaller chunks (done by Sruthija Pagolu)
    We tried to implement the sentiment analysis algorithm and gained the polarities of each conversations of Chat GPT and User (done by Akhil Raj Tirumalasetty)
    We finally threshold values of dividing the positive and negative satisfaction results, produced results for each language of chat GPT (done by Manikanta Mamidi)

---
+++ README.md
@@ -0,0 +1,21 @@
+This is a miniming challenge 2023.
+
+Contributers:
+Akhil Raj Tirumalasetty (AT21BO@fsu.edu)
+Manikanta Mamidi (MM23BM@fsu.edu)
+Sruthijha Pagolu (SP23BU@fsu.edu)
+
+Our Research Questions:
+
+I.	    To what extent can ChatGPT effectively respond to various programming languages?
+II.     To what extent can ChatGPT effectively respond to various programming languages?
+III.	At what stage of the conversation on average does ChatGPT fails in particular coding language?
+
+
+Our Introduction Video:
+https://youtu.be/L0FowLOs9Yk
+
+The dataset we used is JSON files and will be in the JSON folder:
+We use Powershell. Python for our project.
+
+
---
+++ jsonFilter.ps1
@@ -0,0 +1,33 @@
+﻿# Specify the path to your JSON file
+$filePath = 'C:\Education\SE\20230727_200102_file_sharings.json'
+
+# Read the content of the JSON file
+$jsonContent = Get-Content -Path $filePath 
+# Convert the JSON content to a PowerShell object
+$jsonObject = $jsonContent | ConvertFrom-Json
+
+#filtering 1
+$filter1 = $jsonObject.Sources | Select-Object -Property 'ChatgptSharing'
+
+$filter2 = @()
+
+$i = 0
+$k = 1
+foreach($chatSharingProperty in $filter1){
+    foreach($obj in $chatSharingProperty.ChatgptSharing){
+        if($i -eq 100){ 
+            $outputString = "Writing total count of " + $filter2.count +" into the json file" 
+            Write-Output $outputString
+            Write-Output $filter2 |ConvertTo-Json -Depth 5 | Out-File "C:\Education\SE\jsonFiles\filteredJSON$k.json" 
+            $k+=1
+            $i = 0
+            $filter2 = @()
+        }
+        if($obj.Status -eq 200){
+            $i+=1
+            $temp = $obj | Select-Object * -ExcludeProperty 'HTMLContent','Mention'
+            $filter2 += $temp
+        }
+    }    
+}
+
